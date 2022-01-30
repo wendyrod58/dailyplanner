19b4bc5d468101ee7567affd86172ec31cd918ac
Wendy Rodriguez <wendyrod58@gmail.com>
2022-01-30 15:58:08 -0800

    added bootstrap table and moment js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,30 @@
 var rightNow = moment().format("MMMM Do, YYYY - hh:mm:ss a");
-console.log(rightNow);
+
+//Grab HTML ELEMENT 
+var currentEl = document.getElementById("currentDay");
+
+//display text on the elemnt 
+currentEl.textContent = rightNow;
 
 
-var tomorrow = moment().add(1, "day").format("dddd, MM-D-YYYY [at] hh:mm:ss A");
-console.log(tomorrow);
+function saveEvent() {
+    console.log("save button click", this);
+    //grab the hour and decription 
+    var description = document.getElementsByClassName("description");
+    console.log(description[0].value);
+    var hour = document.getElementsByClassName("hour");
+    console.log(hour[0].textContent);
 
-var pastDate = moment("12-01-1999", "MM-DD-YYYY").format("dddd, MM/DD/YY");
-console.log(pastDate);
+    //set it to locastorage 
+    localStorage.setItem(hour[0].textContent, description[0].value) 
+}
 
-rightNow.append()
+//  grab save button
+var saveBtn = document.getElementsByClassName("saveBtn");
+console.log("grabbing save button ", saveBtn);
 
+//Add an event listiner to the button 
+
+saveBtn[0].onclick = saveEvent;
+saveBtn[1].onclick = saveEvent;
+// saveBtn[0].onclick = saveEvent; 
